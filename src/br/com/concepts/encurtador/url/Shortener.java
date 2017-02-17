3f090386a18e17c96d66c212ea5cefdf3014209e
Alan Araujo <alan.santos@lasa.com.br>
2017-02-17 17:58:46 -0200

    teste

---
+++ src/br/com/concepts/encurtador/url/Shortener.java
@@ -0,0 +1,81 @@
+package br.com.concepts.encurtador.url;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import br.com.concepts.model.GerandoPDF;
+
+
+public class Shortener {
+	
+	private static final String BASE_URL_SHORT = "http://shortener";
+	//XYhakR
+
+	public static void main(String[] args) {
+		
+		Shortener s = new Shortener();
+		
+		System.out.println(s.geraUrlShort("alan.com.br","qualquer_coisa"));
+		System.out.println(s.geraUrlShort("alan.com.br"));
+		
+		List<String> letras = Arrays.asList("A", "B", "C");
+		Collections.shuffle(letras);
+		System.out.println(letras);	
+		
+		System.out.println(s.gerarString());
+		
+		s.gera();
+		s.gera();
+		s.gera();
+		s.gera();
+
+	}
+	
+	public String geraUrlShort(String url){
+		
+		return geraUrlShort(url, null);
+		
+	}
+
+	
+	public String geraUrlShort(String url, String customAlias){
+		
+		String custom = "";
+		
+		if (!isEmpty(customAlias)){
+			//TODO busca no banco se existe
+			
+			custom = customAlias;
+		}else{
+			custom = gerarString();
+		}
+		
+		return BASE_URL_SHORT + "/u/"+custom;
+		
+	}
+	
+	public String gerarString(){
+		return gerarString(7,"ABCDEFGHIJKLMNPQRSTUVXZWYabcdefghijklmnopqrstuvxzwy");
+	}	
+	
+	public String gerarString(int tamanho, String letras) {
+		Random random = new Random();
+		StringBuilder saida = new StringBuilder();
+		for (int i = 0; i < tamanho; i++) {
+			saida.append(letras.charAt(random.nextInt(letras.length())));
+		}
+		return saida.toString();
+	}
+	
+	public void gera(){
+		Random random = new Random();
+		System.out.println(random.nextInt(10));
+	}
+	
+	public boolean isEmpty(String valor){
+		return valor == null || valor.isEmpty();
+	}
+
+}
